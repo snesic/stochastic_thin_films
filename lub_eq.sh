08258fbcbdf2f1882f61ffeadf98bf9ca34beee6
Svetozar Nesic <snesic@gmail.com>
2017-02-01 16:34:01 +0100

    Run sim

--- lub_eq.sh
+++ lub_eq.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-g++ -c random_no_generators.cpp -Wno-deprecated -w -O4
+g++ -c film.cpp -Wno-deprecated -w -O4
 g++ -c read_write_msg.cpp -Wno-deprecated -w -O4
 g++ -c newton-raphson_tools.cpp -Wno-deprecated -w -O4
 g++ -c initial_conditions.cpp -Wno-deprecated -w -O4
-g++ -c create_forces.cpp -Wno-deprecated -w -O4
-g++ -c simulation.cpp -Wno-deprecated -w -O4
-g++ instabilities.cpp simulation.o create_forces.o newton-raphson_tools.o initial_conditions.o random_no_generators.o read_write_msg.o -o run_thin_film -Wno-deprecated -w -O4
+g++ instabilities.cpp film.o newton-raphson_tools.o initial_conditions.o read_write_msg.o -o run_thin_film -Wno-deprecated -w -O4
 rc=$?
 echo $rc
 if [[ $rc -eq 0 ]]; then
